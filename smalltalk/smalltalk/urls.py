a8828f3dc52f47430e2f4922facb4e12881ea696
Shauna <shaunagm@gmail.com>
2015-06-29 14:47:48 -0700

    Update urls.py to display index view

--- smalltalk/smalltalk/urls.py
+++ smalltalk/smalltalk/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+
+from main import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'smalltalk.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.IndexView.as_view(), name='home'),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
