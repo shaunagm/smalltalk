f4819fc04b16ea7fe39fa4a5fc8e601e07530c60
Shauna <shaunagm@gmail.com>
2015-06-29 15:22:18 -0700

    Create contact model, form and display in view/template

--- smalltalk/main/views.py
+++ smalltalk/main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .forms import ContactForm
+
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['ContactForm'] = ContactForm()
+        return context
