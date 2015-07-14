ab5f6c3c6648db813ac18499d848014d485b8b1b
Shauna <shaunagm@gmail.com>
2015-07-14 11:29:26 -0400

    Minor refactor smalltalk.js

--- smalltalk/main/static/smalltalk.js
+++ smalltalk/main/static/smalltalk.js
@@ -8,36 +8,60 @@ $( document ).ready(function() {
         $("#group_form_container").show();
     });
 
-    $('#new_contact_submit').click(function() {
+    $("#new_contact_submit").click(new_contact_submit());
 
-        $.ajax({
-            url: '/new_contact',
-            type: 'POST',
-            data: {'name': $("#id_contact-name").val(),
-                'details': $("#id_contact-details").val()},
-            dataType: 'json',
-            beforeSend: function(xhr, settings) {
-                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-            },
-            success: function(data, textStatus, jqXHR) {
-                var d = $.parseJSON(data);
-                if (d.status == "success") {
-                    $("#contact_form_container").hide();
-                    $("#new_contact_message").html("You have added a new contact: <a href='" +
-                        d.url + "'>" + d.name + "</a>.");
-                } else {
-                    $("#new_contact_message").html(d.message);
-                }
-            },
-            error: function (response) {
-                console.log(response);
-                $("#new_contact_message").html("There was an error with the site.");
+    $("#new_group_submit").click(new_group_submit());
+
+
+});
+
+
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
-        });
+        }
+    }
+    return cookieValue;
+};
 
-    });
+function new_contact_submit() {
 
-    $('#new_group_submit').click(function() {
+    $.ajax({
+        url: '/new_contact',
+        type: 'POST',
+        data: {'name': $("#id_contact-name").val(),
+        'details': $("#id_contact-details").val()},
+        dataType: 'json',
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        },
+        success: function(data, textStatus, jqXHR) {
+            var d = $.parseJSON(data);
+            if (d.status == "success") {
+                $("#contact_form_container").hide();
+                $("#new_contact_message").html("You have added a new contact: <a href='" +
+                d.url + "'>" + d.name + "</a>.");
+            } else {
+                $("#new_contact_message").html(d.message);
+            }
+        },
+        error: function (response) {
+            console.log(response);
+            $("#new_contact_message").html("There was an error with the site.");
+        }
+};
+
+function new_group_submit() {
 
         $.ajax({
             url: '/new_group',
@@ -64,23 +88,4 @@ $( document ).ready(function() {
             }
         });
 
-    });
-
-});
-
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
 };
